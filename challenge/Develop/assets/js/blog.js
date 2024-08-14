c6052bd03b60d768c91791fc69f59972156fc1ed
Jacob Menlove <jacobmenlove1@gmail.com>
2024-08-14 01:29:10 -0400

    10th push. more updates and fixes

--- challenge/Develop/assets/js/blog.js
+++ challenge/Develop/assets/js/blog.js
@@ -1,28 +1,40 @@
-const main = document.getElementById('blog-posts');
+const main = document.querySelector('main');
+const toggleButton = document.getElementById('toggle');
+const backButton = document.getElementById('back');
 
+function renderPosts() {
+  let posts = JSON.parse(localStorage.getItem('posts')) || [];
 
-if (posts.length === 0) {
+  if (posts.length === 0) {
     main.innerHTML = '<p>No Blog posts yet...</p>';
     return;
   }
 
+  main.innerHTML = '';
+
   posts.forEach(post => {
-    const article = document.createElement('article');
-    article.innerHTML = `
+    const postElement = document.createElement('article');
+    postElement.innerHTML = `
       <h2>${post.title}</h2>
       <blockquote>By ${post.username}</blockquote>
       <p>${post.content}</p>
     `;
-    main.appendChild(article);
+    main.appendChild(postElement);
   });
 }
 
+toggleButton.addEventListener('click', function() {
+  document.body.classList.toggle('dark');
+  localStorage.setItem('mode', document.body.classList.contains('dark') ? 'dark' : 'light');
+});
 
-function getPosts() {
-    return JSON.parse(localStorage.getItem('posts')) || []; 
-}
+document.addEventListener('DOMContentLoaded', function() {
+  if (localStorage.getItem('mode') === 'dark') {
+    document.body.classList.add('dark');
+  }
+  renderPosts();
+});
 
-document.addEventListener('DOMContentLoaded', () => {
-    const posts = getPosts();
-    renderPosts(posts);
-  });
+backButton.addEventListener('click', function() {
+  location.assign('index.html');
+});
