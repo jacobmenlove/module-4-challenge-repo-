c6d4cd5ecf136df40e035cc72f2e2c620f3a7aa1
Jacob Menlove <jacobmenlove1@gmail.com>
2024-08-14 00:54:18 -0400

    7th push. logic.js

--- challenge/Develop/assets/js/logic.js
+++ challenge/Develop/assets/js/logic.js
@@ -1,3 +1,15 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
 
 // TODO: Create functions to read and write from local storage
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('toggle');
+    const currentMode = localStorage.getItem('mode') || 'light';
+    document.body.classList.add(currentMode);
+  
+    toggle.addEventListener('click', () => {
+      const newMode = document.body.classList.contains('light') ? 'dark' : 'light';
+      document.body.classList.replace(currentMode, newMode);
+      localStorage.setItem('mode', newMode);
+    });
+  });
